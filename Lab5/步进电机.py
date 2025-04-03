7fb968b1a28544c3db37cf2e35bc9b865c41588b
sherlocknieh <sherlocknieh@gmail.com>
2025-04-03 13:45:38 +0800

    完成步进电机

--- Lab5/步进电机.py
+++ Lab5/步进电机.py
@@ -1,37 +1,28 @@
-from gpiozero import OutputDevice, Button
+from gpiozero import OutputDevice
 from time import sleep
-from signal import pause
-
 
 A1 = OutputDevice(17) # A+
 A0 = OutputDevice(18) # A-
 B1 = OutputDevice(27) # B+
 B0 = OutputDevice(22) # B-
 
-motor = [A1, B1, A0, B0]                # 电机引脚
-button = Button(21, bounce_time=0.05)   # 按键
-stopped = False                  # 停止标志
+motor = [A1, B1, A0, B0] # 轮转顺序
 
+# steps: 前进步数
+# speed: 速度(步/秒)
+# direction: 方向(1正转，-1反转)
 def step(steps=1000, speed=100, direction=1):
-    # 速度转化为延时
-    delay = 1/speed
-    # 归零
-    for i in motor:
-        i.off()
+    delay = 1/speed    # 速度转化为延时
     # 开始转动
-    index = 0
-    while steps and not stopped:
-        motor[index-1].off()
-        motor[index].on()
-        index = (index + direction)%4
+    i = 0
+    while steps > 0:
+        for j in range(4):
+            motor[j].off() # 先全部关闭
+        motor[i].on()      # 再打开当前引脚
+        i = (i + direction)%4
         steps -= 1
         sleep(delay)
 
-def toggle_stopped():
-    global stopped
-    stopped = not stopped
-
-button.when_pressed = toggle_stopped
-
 if __name__ == "__main__":
-    step()
+
+    step(steps=22, speed=10, direction=-1) # 转360°
